5cc832db78b282190d20466fbc85506aec96aa9c
sayali1098 <sauamritkar@gmail.com>
2020-07-01 22:28:42 +0530

    FSD0620B102

--- src/model/Charizard.java
+++ src/model/Charizard.java
@@ -19,3 +19,47 @@ package model;
 //type = "FIRE"
 //evolution = "CHARMELEON"
 //baseExp = "240" 
+
+public class Charizard extends Pokemon{
+	private String characteristics;
+	private String type;
+	private String evolution;
+	private String baseExp;
+	
+	public String getCharacteristics() {
+		return characteristics;
+	}
+	public void setCharacteristics(String characteristics) {
+		this.characteristics = characteristics;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getEvolution() {
+		return evolution;
+	}
+	public void setEvolution(String evolution) {
+		this.evolution = evolution;
+	}
+	public String getBaseExp() {
+		return baseExp;
+	}
+	public void setBaseExp(String baseExp) {
+		this.baseExp = baseExp;
+	}
+
+	public Charizard(String pokemonName, int pokemonNumber, String characteristics, String type, String evolution,
+			String baseExp) {
+		super(pokemonName, pokemonNumber);
+		pokemonName = "Charizard";
+	    pokemonNumber = 006;
+		this.characteristics = "Charizard is fit and strong and has the ability to soar up to ridiculous heights.With a single breath of fire, it can burn down forests and melt gigantic glaciers.";
+		this.type = "FIRE";
+		this.evolution = "CHARMELEON";
+		this.baseExp = "240";
+	}
+	
+}
--- src/model/Pokemon.java
+++ src/model/Pokemon.java
@@ -7,4 +7,27 @@ package model;
 //
 //Generate appropriate getters and setters.
 //Generate a two-argument constructor in the Pokemon class
-
+public class Pokemon{
+	private String pokemonName;
+	private int pokemonNumber;
+	
+	public String getPokemonName() {
+		return pokemonName;
+	}
+	public void setPokemonName(String pokemonName) {
+		this.pokemonName = pokemonName;
+	}
+	public int getPokemonNumber() {
+		return pokemonNumber;
+	}
+	public void setPokemonNumber(int pokemonNumber) {
+		this.pokemonNumber = pokemonNumber;
+	}
+	
+	public Pokemon(String pokemonName, int pokemonNumber) {
+		this.pokemonName = pokemonName;
+		this.pokemonNumber = pokemonNumber;
+	}
+	
+	
+}
